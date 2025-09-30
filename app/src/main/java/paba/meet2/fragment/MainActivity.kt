e77d5a6a2a684090389e1dc50e3b42397d21c6ee
ChristopherLouisT <c14230232@john.petra.ac.id>
2025-09-30 18:17:42 +0700

    Meet 08 - Fragment

--- app/src/main/java/paba/meet2/fragment/MainActivity.kt
+++ app/src/main/java/paba/meet2/fragment/MainActivity.kt
@@ -1,10 +1,13 @@
 package paba.meet2.fragment
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val BtnFragment1 = findViewById<Button>(R.id.btnFragmentSatu)
+        val BtnFragment2 = findViewById<Button>(R.id.btnFragmentDua)
+
+        if (savedInstanceState == null) {
+            replaceFragment(fSatu())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
